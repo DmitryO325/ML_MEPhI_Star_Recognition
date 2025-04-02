26915984271618b36dce4b290a3df9d3a04f1a06
Rodion Smolyar <s1rodion123@gmail.com>
2025-04-02 21:36:48 +0300

    got a good model

--- main.py
+++ main.py
@@ -17,46 +17,46 @@ df.drop('type', inplace=True, axis=1)
 # print(df.isnull().sum())
 df.drop(df.index[:5], inplace=True)
 
-# # some fancy graphics
-#
-# # correlation
-# plt.figure()
-# corr_matrix = df.corr()
-# sns.heatmap(corr_matrix, annot=True)
-# plt.show() # we can drop Bmag, gpmag, rpmag, ipmag since they correlate with Vmag
-#
-# df.drop(['Bmag', 'gpmag', 'rpmag', 'ipmag'], inplace=True, axis=1)
-#
-# # histogram
-# df.drop(['present'], axis=1).hist()
-# plt.tight_layout()
-# plt.show()
-#
-# plt.figure()
-# plt.boxplot(df['nobs'])
-# plt.show() # majority is < 30, but some values are very big
-#
-# plt.figure()
-# plt.violinplot(df[df['nobs'] < 30]['nobs'])
-# plt.show() # most of the values are close to zero and less than 10
-#
-# plt.figure()
-# plt.violinplot(df['RAJ2000'])
-# plt.show() # interesting distribution
+# some fancy graphics
+
+# correlation
+plt.figure()
+corr_matrix = df.corr()
+sns.heatmap(corr_matrix, annot=True)
+plt.show() # we can drop Bmag, gpmag, rpmag, ipmag since they correlate with Vmag
+
+df.drop(['Bmag', 'gpmag', 'rpmag', 'ipmag'], inplace=True, axis=1)
+
+# histogram
+df.drop(['present'], axis=1).hist()
+plt.tight_layout()
+plt.show()
+
+plt.figure()
+plt.boxplot(df['nobs'])
+plt.show() # majority is < 30, but some values are very big
+
+plt.figure()
+plt.violinplot(df[df['nobs'] < 30]['nobs'])
+plt.show() # most of the values are close to zero and less than 10
+
+plt.figure()
+plt.violinplot(df['RAJ2000'])
+plt.show() # interesting distribution
 
 # the last one, I swear
 
-# plt.figure()
-# tsne = TSNE(random_state=42)
-# X_repr = tsne.fit_transform(df)
-# plt.scatter(X_repr[df['present']==1, 0],
-#             X_repr[df['present']==1, 1], alpha=.2, c='blue',
-#            label='constant')
-# plt.scatter(X_repr[df['present']==0, 0],
-#             X_repr[df['present']==0, 1], alpha=.2, c='orange',
-#            label='pulsar')
-# plt.legend()
-# plt.show()
+plt.figure()
+tsne = TSNE(random_state=42)
+X_repr = tsne.fit_transform(df)
+plt.scatter(X_repr[df['present']==1, 0],
+            X_repr[df['present']==1, 1], alpha=.2, c='blue',
+           label='constant')
+plt.scatter(X_repr[df['present']==0, 0],
+            X_repr[df['present']==0, 1], alpha=.2, c='orange',
+           label='pulsar')
+plt.legend()
+plt.show()
 
 # let's go
 
@@ -83,7 +83,7 @@ def count_metrics(y, y_pred):
 
 # linear classification
 
-# from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression
 
 X = df[[x for x in df.columns if x != 'present']]
 y = df['present']
@@ -91,52 +91,52 @@ y = df['present']
 scaler = RobustScaler()
 X_scaled = scaler.fit_transform(X)
 
-# lr = LogisticRegression(max_iter=300, class_weight='balanced')
-#
-# # cross-validation
-# cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-# y_pred = cross_val_predict(lr, X_scaled, y, cv=cv)
+lr = LogisticRegression(max_iter=300, class_weight='balanced')
+
+# cross-validation
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+y_pred = cross_val_predict(lr, X_scaled, y, cv=cv)
 
 
-# count_metrics(y, y_pred)
+count_metrics(y, y_pred)
 
-# # kNN, плохо подходит для несбалансированного датасета
-# from sklearn.neighbors import KNeighborsClassifier
-# knn = KNeighborsClassifier(n_neighbors=3, weights='distance')
-#
-# cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-# y_pred = cross_val_predict(knn, X_scaled, y, cv=cv)
-#
-# count_metrics(y, y_pred)
-#
-# # decision tree
-# from sklearn.tree import DecisionTreeClassifier
-# tree = DecisionTreeClassifier(criterion="entropy", class_weight='balanced')
-#
-# cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-# y_pred = cross_val_predict(tree, X_scaled, y, cv=cv)
-#
-# count_metrics(y, y_pred)
+# kNN, плохо подходит для несбалансированного датасета
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=3, weights='distance')
+
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+y_pred = cross_val_predict(knn, X_scaled, y, cv=cv)
+
+count_metrics(y, y_pred)
+
+# decision tree
+from sklearn.tree import DecisionTreeClassifier
+tree = DecisionTreeClassifier(criterion="entropy", class_weight='balanced')
+
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+y_pred = cross_val_predict(tree, X_scaled, y, cv=cv)
+
+count_metrics(y, y_pred)
 
 # random forest
-# from sklearn.ensemble import RandomForestClassifier
-#
-# rfc = RandomForestClassifier(class_weight='balanced')
-#
-# cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-# y_pred = cross_val_predict(rfc, X_scaled, y, cv=cv)
-#
-# count_metrics(y, y_pred)
+from sklearn.ensemble import RandomForestClassifier
+
+rfc = RandomForestClassifier(class_weight='balanced')
+
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+y_pred = cross_val_predict(rfc, X_scaled, y, cv=cv)
+
+count_metrics(y, y_pred)
 
 # gradient boosting - recall 0.16
 from sklearn.ensemble import GradientBoostingClassifier
-#
-# gb = GradientBoostingClassifier()
-#
-# cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-# y_pred = cross_val_predict(gb, X_scaled, y, cv=cv)
-#
-# count_metrics(y, y_pred) # acc 0.91, prec 0.86, rec 0.16, f1 0.27
+
+gb = GradientBoostingClassifier()
+
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+y_pred = cross_val_predict(gb, X_scaled, y, cv=cv)
+
+count_metrics(y, y_pred) # acc 0.91, prec 0.86, rec 0.16, f1 0.27
 
 # подбор параметров
 from imblearn.over_sampling import SMOTE
@@ -147,11 +147,11 @@ pipeline = Pipeline([
     ("gb", GradientBoostingClassifier())
 ])
 
-# gb = GradientBoostingClassifier()
-# cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-# y_pred = cross_val_predict(pipeline, X_scaled, y, cv=cv)
-#
-# count_metrics(y, y_pred) # 0.86 acc, 0.4 prec, 0.72 rec, 0.52 F1
+gb = GradientBoostingClassifier()
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+y_pred = cross_val_predict(pipeline, X_scaled, y, cv=cv)
+
+count_metrics(y, y_pred) # 0.86 acc, 0.4 prec, 0.72 rec, 0.52 F1
 
 from sklearn.model_selection import GridSearchCV
 
@@ -165,9 +165,9 @@ cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 grid_search = GridSearchCV(pipeline, params, scoring="recall", cv=cv, n_jobs=-1)
 grid_search.fit(X_scaled, y)
 
-print("Лучшие параметры:", grid_search.best_params_)
+print("Лучшие параметры:", grid_search.best_params_) #max_depth=12, k_neighbors=5
 
 best_model = grid_search.best_estimator_
 
 y_pred = cross_val_predict(best_model, X_scaled, y, cv=cv)
-count_metrics(y, y_pred)
+count_metrics(y, y_pred) # acc 0.94, prec 0.68, rec 0.77, f1 0.72
